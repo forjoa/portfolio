394d4329750142609d73aca2b05e986ad3012443
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-09-03 21:24:25 +0200

    'About Me' finished

--- general/src/About.jsx
+++ general/src/About.jsx
@@ -15,6 +15,12 @@ import js from './img/js.png'
 import react from './img/react.png'
 import node from './img/node.png'
 
+// studied
+import ccc from './img/ccc.svg'
+import udemy from './img/udemy.png'
+import yt from './img/yt.jpg'
+import chatgpt from './img/chatgpt.png'
+
 const About = () => {
     useEffect(() => {
         AOS.init()
@@ -46,9 +52,9 @@ const About = () => {
                         <h1 className="about-title">?Qué prefiero¿</h1>
                         <p className="about-text">
                             Todo, o la gran mayoría de cosas que he aprendido en el ámbito de la programación han sido
-                            gracias a Java, empezando por la lógica de programación, tipos de variables, bucles y 
+                            gracias a Java, empezando por la lógica de programación, tipos de variables, bucles y
                             estructuras de control. Sin embargo, luego de intentar nuevas cosas, casi todos mi proyectos
-                            realizados, están hechos con React.js en el frontend y Node.js en la parte de backend 
+                            realizados, están hechos con React.js en el frontend y Node.js en la parte de backend
                         </p>
                     </div>
                     <div className="about-images">
@@ -56,6 +62,24 @@ const About = () => {
                         <img src={java} />
                         <img src={node} />
                         <img src={react} />
+                    </div>
+                </section>
+                <section className='learning' data-aos="zoom-in-down">
+                    <div className="about-content">
+                        <h1 className="about-title">¿Cómo aprendí?</h1>
+                        <p className="about-text">
+                            Mis inicios como programador se dieron por la FP de Desarrollo de Aplicaciones Web, iniciando en 2022,
+                            con el lenguaje de enseñanza siendo Java, además de los principios básicos de HTML y CSS. Luego, por 
+                            mis ansías de aprender, continué con cursos en Udemy más a fondo. Cada cosa que necesito o me gustaría 
+                            aprender, las encuentro en YouTube; y en caso de ser una duda extrema, siempre está bien consultar a 
+                            ChatGPT.
+                        </p>
+                    </div>
+                    <div className="about-images">
+                        <img src={ccc} />
+                        <img src={udemy} />
+                        <img src={yt} />
+                        <img src={chatgpt} />
                     </div>
                 </section>
             </main>
