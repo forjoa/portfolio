2cf4a975d599679973ddd969244eb55cc870820a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-10 10:38:15 +0200

    Everything clean

--- general/src/App.jsx
+++ general/src/App.jsx
@@ -1,18 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import NavBar from './NavBar.jsx'
-import Home from './Home.jsx'
-import About from './About.jsx'
-import Notes from './Notes.jsx'
 
 function App() {
 
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path='/notes' element={<Notes />} />
+        <Route exact path="/" element={<></>} />
       </Routes>
     </Router>
   )
