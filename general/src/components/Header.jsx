6637060ad4c50823171aff04ffe67e5c9a645851
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-13 03:12:02 +0200

    Header almost finished

--- general/src/components/Header.jsx
+++ general/src/components/Header.jsx
@@ -3,6 +3,7 @@ import pandaIcon from '../img/icons/panda-black-and-white-with-glasses.svg'
 
 // components
 import { Link } from 'react-router-dom'
+import { IconArrowUpRight } from '@tabler/icons-react'
 
 // styles
 import '../styles/Header.css'
@@ -16,8 +17,8 @@ const Header = () => {
                 <Link to='/'>About</Link>
             </nav>
             <ul className='header-list'>
-                <li><a href='https://www.github.com/forjoa'>GitHub</a></li>
-                <li><a href='#'>Resume</a></li>
+                <li><a href='https://www.github.com/forjoa'>GitHub <IconArrowUpRight size={20}/></a></li>
+                <li><a href='#'>Resume <IconArrowUpRight size={20}/></a></li>
             </ul> 
         </header>
     )
