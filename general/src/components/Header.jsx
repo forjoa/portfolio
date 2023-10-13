d2f8948f7832a438ecfc34ddc4ca0e13cee4c88d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-10-13 02:33:20 +0200

    Header aligned

--- general/src/components/Header.jsx
+++ general/src/components/Header.jsx
@@ -1,19 +1,24 @@
+// images
 import pandaIcon from '../img/icons/panda-black-and-white-with-glasses.svg'
 
+// components
 import { Link } from 'react-router-dom'
+
+// styles
+import '../styles/Header.css'
 
 const Header = () => {
     return (
         <header>
-            <img src={pandaIcon} />
+            <img src={pandaIcon} className='panda-icon'/>
             <nav>
                 <Link to='/'>Home</Link>
                 <Link to='/'>About</Link>
             </nav>
-            <ul>
+            <ul className='header-list'>
                 <li><a href='https://www.github.com/forjoa'>GitHub</a></li>
                 <li><a href='#'>Resume</a></li>
-            </ul>
+            </ul> 
         </header>
     )
 }
