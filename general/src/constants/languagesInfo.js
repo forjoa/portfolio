610753cfe20e045c556bf6ec89ec5c3f6f74d071
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-04-27 09:12:02 +0200

    add next.js in languages

--- general/src/constants/languagesInfo.js
+++ general/src/constants/languagesInfo.js
@@ -7,40 +7,45 @@ import cssIcon from '../img/icons/css.svg'
 import jsIcon from '../img/icons/js.svg'
 import phpIcon from '../img/icons/php.svg'
 import typescriptIcon from '../img/icons/typescript.svg'
+import nextIcon from '../img/icons/next.svg'
 
 const languagesInfo = {
-    node: {
-        icon: nodeIcon,
-        name: 'Node.js',
-    },
-    react: {
-        icon: reactIcon,
-        name: 'React.js',
-    },
-    sql: {
-        icon: sqlIcon,
-        name: 'SQL',
-    },
-    html: {
-        icon: htmlIcon,
-        name: 'HTML'
-    },
-    css: {
-        icon: cssIcon,
-        name: 'CSS'
-    },
-    js: {
-        icon: jsIcon,
-        name: 'JavaScript'
-    },
-    php: {
-        icon: phpIcon,
-        name: 'PHP'
-    },
-    ts: {
-        icon: typescriptIcon,
-        name: 'TypeScript'
-    }
+  next: {
+    icon: nextIcon,
+    name: 'Next.js',
+  },
+  node: {
+    icon: nodeIcon,
+    name: 'Node.js',
+  },
+  react: {
+    icon: reactIcon,
+    name: 'React.js',
+  },
+  sql: {
+    icon: sqlIcon,
+    name: 'SQL',
+  },
+  html: {
+    icon: htmlIcon,
+    name: 'HTML',
+  },
+  css: {
+    icon: cssIcon,
+    name: 'CSS',
+  },
+  js: {
+    icon: jsIcon,
+    name: 'JavaScript',
+  },
+  php: {
+    icon: phpIcon,
+    name: 'PHP',
+  },
+  ts: {
+    icon: typescriptIcon,
+    name: 'TypeScript',
+  },
 }
 
 export default languagesInfo
