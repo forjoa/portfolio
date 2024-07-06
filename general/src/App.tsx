d2578217e351eff7a6cb2c48c137d8eda1f8b8e9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-06 22:05:34 +0200

    style: format code

--- general/src/App.tsx
+++ general/src/App.tsx
@@ -5,11 +5,10 @@ import Home from './components/Home.tsx'
 import About from './components/About.tsx'
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
     </Router>
